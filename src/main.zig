9586d326ac2a5280b3218ce48d0811fb95adae0d
mjzilver <4ilverj@gmail.com>
2025-02-15 17:08:53 +0100

    add texture loading

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,21 @@ const std = @import("std");
 const rl = @import("raylib");
 const RndGen = std.rand.DefaultPrng;
 
-const Point = struct { x: u16, y: u16, color: rl.Color };
-const PointList = std.ArrayList(Point);
-const defaultColor = rl.Color{
-    .r = 0,
-    .g = 0,
-    .b = 0,
-    .a = 255,
+const Point = struct {
+    x: u16,
+    y: u16,
+    color: rl.Color,
 };
 
-pub fn main() anyerror!void {
-    const screenWidth = 800;
-    const screenHeight = 800;
+const PointList = std.ArrayList(Point);
 
+const pointColor = rl.Color.black;
+const pointRadius = 5;
+const startPointAmount = 5;
+const screenWidth = 800;
+const screenHeight = 800;
+
+pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "Voronoi");
     defer rl.closeWindow();
 
@@ -26,71 +28,108 @@ pub fn main() anyerror!void {
     var points = std.ArrayList(Point).init(allocator);
     defer points.deinit();
 
-    var points_buffer = std.ArrayList(Point).init(allocator);
-    defer points_buffer.deinit();
-
     var rnd = std.rand.DefaultPrng.init(@intCast(std.time.timestamp()));
     const random = rnd.random();
 
-    for (0..2) |_| {
+    for (0..startPointAmount) |_| {
         points.append(make_random_point(random)) catch unreachable;
     }
 
+    var image = generate_image(&points);
+    const texture = rl.loadTextureFromImage(image) catch unreachable;
+    rl.unloadImage(image);
+
+    var needs_update = true;
+
     while (!rl.windowShouldClose()) {
-        if (rl.isMouseButtonReleased(rl.MouseButton.left)) {
-            const p = Point{ .x = @intCast(rl.getMouseX()), .y = @intCast(rl.getMouseY()), .color = random_color(random) };
-
-            std.debug.print("Clicked!", .{});
-
-            points.append(p) catch |err| {
-                std.debug.print("{}", .{err});
-            };
+        if (handle_input(&points, &random)) {
+            needs_update = true;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.up)) {
-            std.debug.print("Up was pressed\n", .{});
-            points.append(make_random_point(random)) catch unreachable;
+        if (needs_update) {
+            image = generate_image(&points);
+            rl.updateTexture(texture, image.data);
+            rl.unloadImage(image);
+            needs_update = false;
         }
 
-        if (rl.isKeyReleased(rl.KeyboardKey.down)) {
-            std.debug.print("Down was pressed\n", .{});
-            _ = points.orderedRemove(random.uintLessThan(usize, points.items.len));
-        }
-
-        render(&points);
+        rl.beginDrawing();
+        rl.drawTexture(texture, 0, 0, rl.Color.white);
+        rl.drawFPS(10, 10);
+        rl.endDrawing();
     }
+
+    rl.unloadTexture(texture);
 }
 
-pub fn render(points: *PointList) void {
-    const start_time = std.time.milliTimestamp();
-    rl.beginDrawing();
+fn handle_input(points: *PointList, random: *const std.Random) bool {
+    if (rl.isMouseButtonReleased(rl.MouseButton.left)) {
+        const p = Point{
+            .x = @intCast(rl.getMouseX()),
+            .y = @intCast(rl.getMouseY()),
+            .color = random_color(random.*),
+        };
 
-    for (0..800) |ux| {
-        for (0..800) |uy| {
+        points.append(p) catch unreachable;
+        return true;
+    }
+
+    if (rl.isKeyReleased(rl.KeyboardKey.up)) {
+        points.append(make_random_point(random.*)) catch unreachable;
+        return true;
+    }
+
+    if (rl.isKeyReleased(rl.KeyboardKey.down)) {
+        if (points.items.len > 0) {
+            _ = points.orderedRemove(random.uintLessThan(usize, points.items.len));
+            return true;
+        }
+    }
+    return false;
+}
+
+fn generate_image(points: *PointList) rl.Image {
+    var image = rl.genImageColor(
+        screenWidth,
+        screenHeight,
+        rl.Color{
+            .r = 0,
+            .g = 0,
+            .b = 0,
+            .a = 255,
+        },
+    );
+
+    for (0..screenHeight) |uy| {
+        for (0..screenWidth) |ux| {
             const x: i32 = @intCast(ux);
             const y: i32 = @intCast(uy);
             const color = get_closest_point(x, y, points).color;
-            rl.drawPixel(x, y, color);
+
+            rl.imageDrawPixel(&image, x, y, color);
         }
     }
 
     for (points.items) |p| {
-        rl.drawCircle(p.x, p.y, 5, defaultColor);
+        rl.imageDrawCircle(
+            &image,
+            p.x,
+            p.y,
+            pointRadius,
+            pointColor,
+        );
     }
 
-    rl.endDrawing();
-
-    const end_time = std.time.milliTimestamp();
-    const elapsed_time = end_time - start_time;
-    std.debug.print("Frame time: {} millisecs\n", .{elapsed_time});
+    return image;
 }
 
-pub fn get_closest_point(x: i32, y: i32, points: *PointList) Point {
-    if (points.items.len < 1) {
+fn get_closest_point(x: i32, y: i32, points: *PointList) Point {
+    // draw a black screen if there are 0 points
+    if (points.items.len == 0) {
         return Point{
             .x = 0,
             .y = 0,
-            .color = defaultColor,
+            .color = pointColor,
         };
     }
     var found_closest = points.items[0];
@@ -111,10 +150,19 @@ pub fn get_closest_point(x: i32, y: i32, points: *PointList) Point {
     return found_closest;
 }
 
-pub fn make_random_point(random: std.Random) Point {
-    return Point{ .x = random.uintLessThan(u16, 800), .y = random.uintLessThan(u16, 800), .color = random_color(random) };
+fn make_random_point(random: std.Random) Point {
+    return Point{
+        .x = random.uintLessThan(u16, 800),
+        .y = random.uintLessThan(u16, 800),
+        .color = random_color(random),
+    };
 }
 
-pub fn random_color(rand: std.Random) rl.Color {
-    return rl.Color{ .r = rand.uintLessThan(u8, 255), .g = rand.uintLessThan(u8, 255), .b = rand.uintLessThan(u8, 255), .a = 255 };
+fn random_color(rand: std.Random) rl.Color {
+    return rl.Color{
+        .r = rand.uintLessThan(u8, 255),
+        .g = rand.uintLessThan(u8, 255),
+        .b = rand.uintLessThan(u8, 255),
+        .a = 255,
+    };
 }
